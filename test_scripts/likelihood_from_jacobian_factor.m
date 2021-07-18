45a0dfc8ced27450a37a143b40c5fc4ac9a3507a
bk82083 <benedikt.klingebiel@gmail.com>
2021-07-18 18:10:47 +0200

    Calculating likelihood from jacobian factor

--- test_scripts/likelihood_from_jacobian_factor.m
+++ test_scripts/likelihood_from_jacobian_factor.m
@@ -3,19 +3,23 @@ import gpmp2.*
 
 pose_fix = noiseModel.Isotropic.Sigma(5, 1);
 
-%Linearized Goal Factor for Point x = 7, y = 2
-A = eye(5)*0.0001
-b = [0.0507003  0.00680837 -0.00274868  0.00149803 5.13813e-06]
+% Linearized Goal Factor for Point x = 7, y = 2
+% A is covariance matrix
+% b is goal point (actual observed measurement)
+% x is point on map
+% b&x have form of (x, y, theta, delta1, delta2)
+A = eye(5)*10
+b = [7  2 -0.00274868  0.00149803 5.13813e-06]*10
 
 % x_vector = zeros(1, 2);
 
 figure;
 hold on;
 
-for x = 60:80
-   for y = 10:250
-       x_adj = x/10
-       y_adj = y/10
+for x = 0:25
+   for y = 0:25
+       x_adj = x/2.5
+       y_adj = y/2.5
        
        x_vector = [x_adj y_adj 0 0 0];
        
